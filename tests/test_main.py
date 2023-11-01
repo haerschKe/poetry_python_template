0dfc63467e886fe8dac47b406a6b1e7d733e468e
haerschKe <kevin.hirschmann1994@gmail.com>
2023-11-01 12:36:12 +0100

    add ci

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,17 @@
 from poetry_python_template.main import main
 
+
 def test_main(capsys):
-    assert main(['--name', 'test']) == 0
+    assert main(["--name", "test"]) == 0
     out, err = capsys.readouterr()
-    # test is a user which does not have any contributions since 2010. 
+    # test is a user which does not have any contributions since 2010.
     # Hopefully this will not change in the future and the test will not break.
-    assert out == 'Hello test!\nYou have 5 repos.\n'
-    assert err == ''
+    assert out == "Hello test!\nYou have 5 repos.\n"
+    assert err == ""
+
 
 def test_main_empty_name(capsys):
-    assert main(['--name', '']) == 1
+    assert main(["--name", ""]) == 1
     out, err = capsys.readouterr()
-    assert out == 'Username cannot be empty\n'
-    assert err == ''
+    assert out == "Username cannot be empty\n"
+    assert err == ""
